02e09093f90de309c610ebd32f13d5ed6a30f2f2
eagleba <40048886+eagleba@users.noreply.github.com>
2018-07-03 07:27:47 +0800

    add

--- 鼎鼎电话机器人/少儿英语培训/smartivr_english_trainging.php
+++ 鼎鼎电话机器人/少儿英语培训/smartivr_english_trainging.php
@@ -4,8 +4,13 @@
 	$data = json_decode($data, TRUE); 
 	
 	$TM_START_MSG = "您好。这里是ABC英语。新学期开始了，我们为小朋友准备了免费的外教英语试听课，想邀请小朋友参加。请问您近期是否想要帮孩子了解英语培训课程呢？";
-	$TM_END_MSG = "好的。感谢您对我工作的支持，希望ABC英语对您的孩子的学习有帮助，祝您生活愉快！再见！"
-	
+	$TM_END_MSG = "好的。感谢您对我工作的支持，希望ABC英语对您的孩子的学习有帮助，祝您生活愉快！再见！";
+	$TMA_MSG = "好的，我是ABC英语，在江南大道附近，我们引进美国幼儿园和小学教材，由资深外教老师任教、让孩子在歌曲、游戏、故事、动画片等活动中轻松学英文。我们的外教英语体验活动通常在周日上午或周三晚上，也可以为您预约其他时间。有英语基础的小朋友，还可以旁听我们周日上午的正式课程，您看孩子哪个时间会比较合适呢？";
+    $TMB_MSG = "好的，我理解。小朋友是还没有开始打算学英语呢？还是已经在其他地方学习了？";
+    $TMC_MSG = "现在英语作为第二语言，孩子在以后的学习中是必然会运用到的，孩子的英语启蒙越早越好呢。我们ABC英语引进美国幼儿园和小学教材，由资深外教老师任教、让孩子在歌曲、游戏、故事、动画片等活动中轻松学英文，很受小朋友的欢迎，您不妨带孩子来免费体验一下。我们还定期举行公益的英语角活动，即使暂时不打算报课，也可以长期参与我们的英语角活动与更多的家长和小朋友交流，培养英语兴趣。您看是否要帮孩子做一个了解呢？";
+    $TMD_MSG = "明白了，我们会尽量根据孩子的特点准备活动内容。稍后我们的老师会通知您活动时间和地址，请您留意一下。好吗？";
+    $TME_MSG = "好的,下次有试听课时我再联系您,您看行吗";
+    
 	if(key_exists("notify",$data) &&
 		key_exists("calleeid",$data) &&
 		key_exists("callerid",$data) &&
@@ -16,6 +21,7 @@
 		$callerid = $data["callerid"];
 		$callid = $data["callid"];
 		$errorcode = 0;
+        
 		if(key_exists("errorcode",$data)){
 			$errorcode = $data["errorcode"];
 		}
@@ -36,36 +42,29 @@
 			$flowdata["break"]=false;
 			$flowdata["retry"]=0;
 			$flowdata["retrytext"]="";
-			$flowdata["keyword"]["yes"]=array("有","好","要","可以","行","没问题");
+			$flowdata["keyword"]["yes"]=array("有","好","要","可以","行","没问题","合适");
 			$flowdata["keyword"]["no"]=array("不","没");
-			$flowdata["keyword"]["not_sure"]=array("还行","再看"，"再说");
+            $flowdata["keyword"]["not_sure"]=array("还行","再看","再说");
 			$flowdata["keyword"]["query"]=array("哪里","什么","你好");
-
+            $flowdata["keyword"]["notime"]=array("没时间","没空","有安排","有课");
 
 			$pause_play_ms = 200; //是否设置自动打断 0，关闭自动打断，其他值（建议 300-1000，或者关闭），检测多少毫秒的声音就打断。
 
-			$flowdata["retrytext"]="您好。这里是ABC英语。新学期开始了，我们为小朋友准备了免费的外教英语试听课，想邀请小朋友参加。请问您近期是否想要帮孩子了解英语培训课程呢？";
-			play_background_asr($pause_play_ms,"您好。这里是ABC英语。新学期开始了，我们为小朋友准备了免费的外教英语试听课，想邀请小朋友参加。请问您近期是否想要帮孩子了解英语培训课程呢？");
+			$flowdata["retrytext"]= $TMA_START;
+			play_background_asr($pause_play_ms,$TMA_START);
 
 		}
 		else if($notify=="playback_result"){
 		
 			if($flowdata["retry"]  > 1){
-				play_after_hangup("好的。感谢您对我工作的支持，希望ABC英语对您的孩子的学习有帮助，再见！");
+				play_after_hangup($TM_END_MSG);
 			}
-
 			else {
-
 				$flowdata["retry"] = $flowdata["retry"] +1;
-
 				playback($flowdata["retrytext"]);
-
 			}
-
-
 		}
 		else if($notify=="asrprogress_notify"){
-
 
 			if($message == "" || $errorcode != 0){
 				 console_playback("resume");
@@ -94,8 +93,6 @@
 					console_playback("resume");
 				}
 			}
-
-
 		}
 		else if($notify=="asrmessage_notify"){
 
@@ -110,34 +107,67 @@
 				if($flowdata["step"]=="TM_START"){
 					if($keyword == "no"){
 						$flowdata["step"]="TMB";
-						$flowdata["retrytext"]="好的，我理解。小朋友是还没有开始打算学英语呢？还是已经在其他地方学习了？";
-						playback("好的，我理解。小朋友是还没有开始打算学英语呢？还是已经在其他地方学习了？");
+						$flowdata["retrytext"]= $TMB_MSG;
+						playback($TMB_MSG);
 					}
 					else if($keyword == "yes"){
 						$flowdata["step"]="TMA";
-						$flowdata["retrytext"]="好的,我们是ABC英语，在江南大道附近，我们引进美国幼儿园和小学教材，由资深外教老师任教、让孩子在歌曲、游戏、故事、动画片等活动中轻松学英文。我们的外教英语体验活动通常在周日上午或周三晚上，也可以为您预约其他时间。有英语基础的小朋友，还可以旁听我们周日上午的正式课程，您看孩子哪个时间会比较合适呢？";
-						play_back("好的,我们是ABC英语，在江南大道附近，我们引进美国幼儿园和小学教材，由资深外教老师任教、让孩子在歌曲、游戏、故事、动画片等活动中轻松学英文。我们的外教英语体验活动通常在周日上午或周三晚上，也可以为您预约其他时间。有英语基础的小朋友，还可以旁听我们周日上午的正式课程，您看孩子哪个时间会比较合适呢？");
-					}
+						$flowdata["retrytext"]= $TMA_MSG;
+						play_back($TMA_MSG);
+                    }
 					else if($keyword == "not_sure"){
 						$flowdata["step"]="TMC";
-						$flowdata["retrytext"]="喂你好，请问你最近有打算买房";
-						playback("我们是售楼部的，请问你最近有打算买房吗");
+						$flowdata["retrytext"]= $TMC_MSG;
+						playback($TMC_MSG);
 					}
 					else{ 	
 						if($playstate){
 							console_playback("resume");
 						}
 						else{
-							$flowdata["step"]="未知_挽留";
-							playback("我们楼盘最近准备开盘，位置非常好，开盘有优惠活动，你需要了解一下吗");
-						}
-						
-
+							$flowdata["step"]="TMC";
+                            $flowdata["retrytext"]= $TMC_MSG;
+	                        playback($TMC_MSG);							
+						}	
 					}
-
 				}
-				else if($flowdata["step"]=="TMB"){
-					if（strstr($message,"没打算"){
+                else if($flowdata["step"]=="TMA"){
+					if($keyword == "yes"){
+						$flowdata["step"]="TMD";
+						$flowdata["retrytext"]= $TMD_MSG;
+						playback($TMD_MSG);
+					}
+					else if($keyword == "notime"){
+						$flowdata["step"]="TME";
+						$flowdata["retrytext"]= $TME_MSG;
+						play_back($TME_MSG);
+                    }
+					else if($keyword == "not_sure"){
+						$flowdata["step"]="TMD";
+						$flowdata["retrytext"]= $TMD_MSG;
+						playback($TMD_MSG);
+					}
+					else{ 	
+						if($playstate){
+							console_playback("resume");
+						}
+						else{
+							$flowdata["step"]="TME";
+                            $flowdata["retrytext"]= $TME_MSG;
+	                        playback($TME_MSG);							
+						}	
+					}
+				}
+                
+                
+                
+                
+                
+                
+                
+                
+				else if($flowdata["step"]=="TMA"){
+					if（strstr($message,"没打算")){
 						$flowdata["step"]="TMC";
 						$flowdata["retrytext"]="现在英语作为第二语言，孩子在以后的学习中是必然会运用到的，孩子的英语启蒙越早越好呢。我们ABC英语引进美国幼儿园和小学教材，由资深外教老师任教、让孩子在歌曲、游戏、故事、动画片等活动中轻松学英文，很受小朋友的欢迎，您不妨带孩子来免费体验一下。我们还定期举行公益的英语角活动，即使暂时不打算报课，也可以长期参与我们的英语角活动与更多的家长和小朋友交流，培养英语兴趣。您看是否要帮孩子做一个了解呢？";
 						playback("现在英语作为第二语言，孩子在以后的学习中是必然会运用到的，孩子的英语启蒙越早越好呢。我们ABC英语引进美国幼儿园和小学教材，由资深外教老师任教、让孩子在歌曲、游戏、故事、动画片等活动中轻松学英文，很受小朋友的欢迎，您不妨带孩子来免费体验一下。我们还定期举行公益的英语角活动，即使暂时不打算报课，也可以长期参与我们的英语角活动与更多的家长和小朋友交流，培养英语兴趣。您看是否要帮孩子做一个了解呢");
@@ -203,15 +233,19 @@
 			}
 		}
 
-
 		foreach ($GLOBALS["flowdata"]["keyword"]["yes"] as $value) {
 			if(strstr($message,$value)){
 				return "yes";
 			}
 		}
+        
+        foreach ($GLOBALS["flowdata"]["keyword"]["not_sure"] as $value) {
+			if(strstr($message,$value)){
+				return "not_sure";
+			}
+		}
 
-
-		return "";
+		return "no_keyword";
 	}
 
 
